dd7734dc708158b80ea90f29525093ab265ed3e2
NishatTasnimSamiha30 <157671665+NishatTasnimSamiha30@users.noreply.github.com>
2024-03-23 10:26:09 +0600

    Add files via upload
    object

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-let A=[50, 107, 'jack', 83, 'killer','son', true];
+// Data collection about current students//
 
+const studentinfo = [
+  {
+    wdd: [
+      { Name: "Sumaya", Id: "wd-002410", Phone: "01777458123" },
+      { Name: "Nishat", Id: "wd-002411", Phone: "01778458198" },
+    ],
+  },
 
-//1.Just show the strings in a new array?
+  {
+    gd: [
+      { Name: "Sajid", Id: "gd-002419", Phone: "01657458123" },
+      { Name: "Pollob", Id: "gd-0024188", Phone: "01978458197" },
+    ],
+  },
 
-let strings = A.filter(y=> typeof y ==='string');
-console.log ("Showing strings in a new array:",strings);
+  {
+    ve: [
+      { Name: "Anu", Id: "ve-002499", Phone: "01657458023" },
+      { Name: "Sohan", Id: "ve-0024988", Phone: "01978458127" },
+    ],
+  },
 
-//2.Create a new array from the numbers and short them?
-
- let numbers = A.filter(item => typeof item ==='number').sort((a, b) => a - b);
-console.log ("Creating a new array from the numbers and shorting them into rows:", numbers);
-
-//3.Make the sting array reverse?
-
-let reversed_strings = strings.reverse();
-console.log ("the string array is reversed:", reversed_strings);
-
-//4.Cobine both array of numbers and strings?
-
-let combine = numbers.concat(strings);
-console.log("after both arrays are combined:", combine);
-
-//5.Delelet 3 items from the 6th item from the combined array?
-
-combine.splice(5, 3);
-console.log("Deleteing 3 items from the 6th item from the combined array:", combine);
-
-//6.Show elements from a which is less than 100?
-
-let less_than_100 = A.filter(item => typeof item === 'number' &&item<100);
-console.log ("showing the elemts which are less than 100:", less_than_100);
-
-//7.From string array remove the last letter and add word 'str' before each of them?
-
-let modified_strings = strings.map(item => item.slice(0, -1) + 'str');
-console.log("showing the result after modification:", modified_strings);
+  {
+    marketing: [
+      { Name: "veronica", Id: "mk-002401", Phone: "01657878023" },
+      { Name: "Sohan", Id: "mk-0024902", Phone: "01978988127" },
+    ],
+  },
+];
+console.log(studentinfo[3]);
